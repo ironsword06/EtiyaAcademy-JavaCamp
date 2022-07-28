324d4c0dce3329dc53747376dc4c936ab830f336
Ali Can Demirkılıç <alicandemirklc@gmail.com>
2022-07-28 20:19:23 +0300

    KosgebProject Commited

---
+++ KosgebProjectws/src/BankEmployee.java
@@ -0,0 +1,18 @@
+
+public class BankEmployee extends User {
+	 Bank bank;
+    
+	public BankEmployee(int id, String username, String password, Bank bank) {
+        super(id, username, password);
+        this.bank = bank;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+}
---
+++ KosgebProjectws/src/Variant.java
@@ -0,0 +1,15 @@
+
+public class Variant {
+	 private Variants variant;
+	    private int id;
+	    public enum Variants{
+	        EMPLOYEE,
+	        CUSTOMER,
+	        BANKEMPLOYEE
+	    }
+
+	    public Variant( int id,Variants variant) {
+	        this.id = id;
+	        this.variant = variant;
+	    }
+}
